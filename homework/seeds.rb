f1d53ee27dc6e1b1c16beb3034bae0bc87110da8
Huascar <huascarmc@hotmail.com>
2017-10-16 18:01:11 +0100

    welcome page added

--- homework/seeds.rb
+++ homework/seeds.rb
@@ -2,6 +2,10 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('models/rps.rb')
 
+get '/' do
+  erb(:home)
+end
+
 get '/:p/:c' do
   rps = RPS.new(params[:p].to_s, params[:c].to_s)
   @game = rps.play()
