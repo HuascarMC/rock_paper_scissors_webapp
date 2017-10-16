701ce23dc1567821c58b8e94f95765abbd73edae
Huascar <huascarmc@hotmail.com>
2017-10-16 17:18:06 +0100

    setting up files

---
+++ homework/models/rps.rb
@@ -0,0 +1,35 @@
+class RPS
+  attr_accessor
+
+  def initialize(player, cpu)
+    @player = player
+    @cpu = cpu
+  end
+
+  def play()
+    while @player != @cpu
+
+      case @player == "R"
+        when @cpu == "S"
+          return "You win"
+        when @cpu == "P"
+          return "You lose"
+      end
+
+      case @player == "P"
+      when @cpu == "R"
+        return "You win"
+      when @player == "S"
+        return "You lose"
+      end
+
+      case @player == "S"
+      when @cpu == "P"
+        return "You win"
+      when @player == "R"
+        return "You lose"
+      end
+    end
+    return "It's a tie!"
+  end
+end
---
+++ homework/seeds.rb
@@ -0,0 +1,9 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('models/rps.rb')
+
+get '/:p/:c' do
+  rps = RPS.new(params[:p].to_s, params[:c].to_s)
+  @game = rps.play()
+  erb(:game)
+end
